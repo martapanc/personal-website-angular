6331ef922be7ec61fe22f3c1bb914b1b5ea3f33f
martapanc <marta.panc@gmail.com>
2023-01-29 22:45:36 +0100

    Setup base components and routing rules

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,23 @@
-import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
+import {NgModule} from '@angular/core';
+import {RouterModule, Routes} from '@angular/router';
+import {HomeComponent} from "./home/home.component";
+import {CvComponent} from "./cv/cv.component";
+import {ProjectsComponent} from "./projects/projects.component";
+import {FoodComponent} from "./food/food.component";
+import {TravelComponent} from "./travel/travel.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', component: HomeComponent},
+  {path: 'home', component: HomeComponent},
+  {path: 'cv', component: CvComponent},
+  {path: 'projects', component: ProjectsComponent},
+  {path: 'travel', component: TravelComponent},
+  {path: 'food', component: FoodComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {
+}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { SidebarComponent } from './shared/sidebar/sidebar.component';
+import { HomeComponent } from './home/home.component';
+import { CvComponent } from './cv/cv.component';
+import { ProjectsComponent } from './projects/projects.component';
+import { TravelComponent } from './travel/travel.component';
+import { FoodComponent } from './food/food.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SidebarComponent,
+    HomeComponent,
+    CvComponent,
+    ProjectsComponent,
+    TravelComponent,
+    FoodComponent
   ],
   imports: [
     BrowserModule,
