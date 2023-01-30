9392e1b26ce866cc9a6adccc0b687c8555180a33
martapanc <marta.panc@gmail.com>
2023-01-31 00:03:45 +0100

    Lint

--- src/app/shared/components/menu-item/menu-item.component.spec.ts
+++ src/app/shared/components/menu-item/menu-item.component.spec.ts
@@ -3,21 +3,20 @@ import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { MenuItemComponent } from './menu-item.component';
 
 describe('MenuItemComponent', () => {
-  let component: MenuItemComponent;
-  let fixture: ComponentFixture<MenuItemComponent>;
+    let component: MenuItemComponent;
+    let fixture: ComponentFixture<MenuItemComponent>;
 
-  beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      declarations: [ MenuItemComponent ]
-    })
-    .compileComponents();
+    beforeEach(async () => {
+        await TestBed.configureTestingModule({
+            declarations: [MenuItemComponent],
+        }).compileComponents();
 
-    fixture = TestBed.createComponent(MenuItemComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
-  });
+        fixture = TestBed.createComponent(MenuItemComponent);
+        component = fixture.componentInstance;
+        fixture.detectChanges();
+    });
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
-  });
+    it('should create', () => {
+        expect(component).toBeTruthy();
+    });
 });
--- src/app/shared/components/social-media-link/social-media-link.component.spec.ts
+++ src/app/shared/components/social-media-link/social-media-link.component.spec.ts
@@ -3,21 +3,20 @@ import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { SocialMediaLinkComponent } from './social-media-link.component';
 
 describe('SocialMediaLinkComponent', () => {
-  let component: SocialMediaLinkComponent;
-  let fixture: ComponentFixture<SocialMediaLinkComponent>;
+    let component: SocialMediaLinkComponent;
+    let fixture: ComponentFixture<SocialMediaLinkComponent>;
 
-  beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      declarations: [ SocialMediaLinkComponent ]
-    })
-    .compileComponents();
+    beforeEach(async () => {
+        await TestBed.configureTestingModule({
+            declarations: [SocialMediaLinkComponent],
+        }).compileComponents();
 
-    fixture = TestBed.createComponent(SocialMediaLinkComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
-  });
+        fixture = TestBed.createComponent(SocialMediaLinkComponent);
+        component = fixture.componentInstance;
+        fixture.detectChanges();
+    });
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
-  });
+    it('should create', () => {
+        expect(component).toBeTruthy();
+    });
 });
