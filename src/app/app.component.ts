e9b3adbc072dfb570249fbba738b34b94190a7ae
martapanc <marta.panc@gmail.com>
2023-01-31 20:20:13 +0100

    Setup firebase hosting

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,8 @@
 import { Component } from '@angular/core';
 import { TranslateService } from '@ngx-translate/core';
+import firebase from 'firebase/compat/app';
+import initializeApp = firebase.initializeApp;
+import { getAnalytics } from 'firebase/analytics';
 
 @Component({
     selector: 'app-root',
@@ -9,6 +12,11 @@ export class AppComponent {
     title = 'Marta Pancaldi';
 
     constructor(translate: TranslateService) {
+        this.setupTranslations(translate);
+        this.setupFirebase();
+    }
+
+    private setupTranslations(translate: TranslateService) {
         // this language will be used as a fallback when a translation isn't found in the current language
         translate.setDefaultLang('en');
 
@@ -20,5 +28,21 @@ export class AppComponent {
         } else if (browserLang) {
             translate.use(browserLang);
         }
+    }
+
+    private setupFirebase() {
+        const firebaseConfig = {
+            apiKey: 'AIzaSyBKThDZUCb0XQ3fG0zNjFX15Bb436UGdoI',
+            authDomain: 'mpancaldi-angular.firebaseapp.com',
+            projectId: 'mpancaldi-angular',
+            storageBucket: 'mpancaldi-angular.appspot.com',
+            messagingSenderId: '643681442779',
+            appId: '1:643681442779:web:01ff99a3be95e37a29e5fe',
+            measurementId: 'G-CVGZV7M5YR',
+        };
+
+        // Initialize Firebase
+        const app = initializeApp(firebaseConfig);
+        const analytics = getAnalytics(app);
     }
 }
