74054b27fd4912f8eadf14defa0a8b59622b8739
Marta <marta.panc@gmail.com>
2023-02-02 22:24:56 +0000

    Add CV component (#13)

--- src/app/module/cv/cv.component.ts
+++ src/app/module/cv/cv.component.ts
@@ -1,8 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { faDownload } from '@fortawesome/free-solid-svg-icons';
+import { getAnalytics, logEvent } from 'firebase/analytics';
 
 @Component({
     selector: 'app-cv',
     templateUrl: './cv.component.html',
-    styleUrls: ['./cv.component.scss'],
+    styleUrls: ['../home/home.component.scss'],
 })
-export class CvComponent {}
+export class CvComponent implements OnInit {
+    icons = { faDownload };
+    cv = {
+        render: 'assets/images/cv/cv.webp',
+        pdf: 'assets/images/cv/MARTA PANCALDI - CV.pdf',
+    };
+
+    ngOnInit(): void {
+        logEvent(getAnalytics(), 'CV visualised');
+    }
+
+    downloadPdfEvent() {
+        logEvent(getAnalytics(), 'CV downloaded');
+    }
+}
