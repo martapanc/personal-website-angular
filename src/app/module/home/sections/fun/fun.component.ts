2fe9d2904ee56ec54126128ccd8aebc107ed5319
martapanc <marta.panc@gmail.com>
2023-02-02 18:55:26 +0100

    Remove gh calendar

--- src/app/module/home/sections/fun/fun.component.ts
+++ src/app/module/home/sections/fun/fun.component.ts
@@ -1,11 +1,8 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-fun',
-  templateUrl: './fun.component.html',
-  styles: [
-  ]
+    selector: 'app-fun',
+    templateUrl: './fun.component.html',
+    styles: [],
 })
-export class FunComponent {
-
-}
+export class FunComponent {}
